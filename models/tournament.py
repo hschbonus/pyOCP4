38fcc540d4c4088b9b24fa2ac6c547b532924529
Hervé <h.schmidt@hotmail.fr>
2026-01-08 15:59:05 +0100

    generate pairs presque ok

--- models/tournament.py
+++ models/tournament.py
@@ -23,25 +23,46 @@ class Tournament:
         self.players = []
 
     def add_player(self, player: Player):
+        """Ajoute player au tournoi"""
         self.players.append(player)
 
     def generate_pairs(self):
+        """Génère les paires servant à la constitution des matchs en fonction du round en cours"""
+        pairs = []
+
         if self.current_round == 0:
             players_copy = self.players.copy()
             random.shuffle(players_copy)
-
-            pairs = []
             for i in range(0, len(players_copy), 2):
                 pairs.append((players_copy[i], players_copy[i + 1]))
-        else : 
 
+        else :
+            players_with_score = []
+            for player in self.players:
+                player_score = self.get_player_score(player)
+                players_with_score.append((player, player_score))
+            players_with_score.sort(key=lambda x: x[1], reverse=True)
+            players_available_for_pairing = []
+            for player in players_with_score:
+                players_available_for_pairing.append(player[0])
+            c = 0
+            while len(players_available_for_pairing) > 2:
+                pairing_result = self.has_played_together(players_available_for_pairing[0], players_available_for_pairing[1+c])
+                if pairing_result :
+                    c += 1
+                else:
+                    pairs.append((players_available_for_pairing[0], players_available_for_pairing[1+c]))
+                    players_available_for_pairing.pop(0)
+                    players_available_for_pairing.pop(1+c)
+                    c = 0
+            pairs.append((players_available_for_pairing[0], players_available_for_pairing[1]))
         return pairs
 
     def create_round(self):
         if len(self.rounds) >= self.rounds_nb:
             raise ValueError("Nombre maximum de tours atteint")
 
-        round_name = f"Round {self.current_round + 1}"
+        round_name = f"{self.name} - Round {self.current_round + 1}"
         new_round = Round(round_name)
 
         pairs = self.generate_pairs()
@@ -69,9 +90,10 @@ class Tournament:
         
         for round in self.rounds:
             for match in round.match_list:
-                if player == match.player
-                # Si le joueur est player2 → ajouter player2_score
-                pass
-        
+                if player == match.player1 :
+                    total_score += match.player1_score
+                elif player == match.player2 :
+                    total_score += match.player2_score
+
         return total_score
 
